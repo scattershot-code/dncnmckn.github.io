f60cae08206b63d55b550ae020c81d3c569eb362
dncnmckn <hello@duncanmckean.com>
2015-11-17 20:04:08 +0100

    Adding Souk Project

--- js/init.js
+++ js/init.js
@@ -7,5 +7,8 @@
 })(jQuery); // end of jQuery name space
 
   $(document).ready(function(){
-      $('.slider').slider({full_width: true});
+      $('.slider').slider({full_width: true,
+                          interval:10000,
+                           height:1200,
+                          });
     });
